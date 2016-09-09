6cabb049f1bb092e6c0b8a1d7878a87da7e65b35
Rohit Daryanani <rohitdaryanani@yahoo.com>
2016-09-09 22:20:12 +0800

    chore: add less support
    - add less
    - mount bower components
    - add bower dependencies

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 // configuration
 //set the static files location /public/img will be /img for users
 app.use(express.static(__dirname + '/public'));
+app.use( '/bower_components', express.static( 'bower_components' ) );
 //log every request to the console
 app.use(morgan('dev'));
 // parse application/x-www-form-urlencoded
